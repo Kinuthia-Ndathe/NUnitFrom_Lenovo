506c2b2b838d47a5591fe19bd6bc4bdf99c99623
Alex Kinuthia (Techno Brain Global FZE) <v-alkinu@microsoft.com>
2024-03-19 15:49:54 +0300

    from Lenovo

---
+++ UnitTest1.cs
@@ -0,0 +1,40 @@
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace PlaywrightDemo01;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions 
+        {
+            Headless = false
+        });
+
+        //Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("http://www.eaapp.somee.com");
+        await page.ClickAsync("text=Login");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EAPP.jpg"
+        });
+
+        await page.FillAsync(selector:"#Username", value:"admin");
+        await page.FillAsync(selector:"#Password", value:"password");
+        await page.ClickAsync(selector:"text=Log in");
+        var isExist = await page.Locator(selector:"text='Employee Details'").IsVisibleAsync(); //the Locator method locates a certain item on the page and returns true if the value defined matches what is specified on this line.
+        Assert.
+    }
+}
